798234b9fd3a52a52e7fe50804ef0fd55aa0bea4
David Goraj <d.goraj@exinit.de>
2017-05-23 14:48:53 +0200

    [task] add now extension to project

---
+++ typo3conf/ext/now/Configuration/TypoScript/Library/0020_lib.logo.ts
@@ -0,0 +1,11 @@
+# Logo in der Menue-Leiste
+lib.logo = IMAGE
+lib.logo {
+    file = EXT:now/Resources/Public/Icons/typo3kurs-logo.png
+    altText = Logo Meine Firma
+    stdWrap.typolink {
+        parameter = 17
+        title = home now
+        ATagParams = class="navbar-brand"
+    }
+}
