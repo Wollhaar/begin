8588f17685e3b62d0c01b8769d84e3fd135d7d09
David Goraj <d.goraj@exinit.de>
2017-05-29 12:55:12 +0200

    [task] edited file for multilanguage

---
+++ typo3conf/ext/now/Configuration/TypoScript/Library/0090_multilanguage.ts
@@ -0,0 +1,19 @@
+config.linkVars = (1-2)
+
+## english
+[globalVar = GP:L=1]
+    config {
+        language = en
+        locale_all = en_GB.utf8
+        sys_language_uid = 1
+    }
+[global]
+
+## polski
+[globalVar = GP:L=2]
+    config {
+        language = pl
+        locale_all = pl_PL.utf8
+        sys_language_uid = 2
+    }
+[global]
