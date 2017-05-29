9795756a499a556e166ed8850930b23e61f16e39
David Goraj <d.goraj@exinit.de>
2017-05-29 14:28:48 +0200

    [task] edited constants.ts for multilanguage

--- typo3conf/ext/now/Configuration/TypoScript/constants.ts
+++ typo3conf/ext/now/Configuration/TypoScript/constants.ts
@@ -1,1 +1,9 @@
+# feLogin
 styles.content.loginform.templateFile = EXT:now/Resources/Private/Templates/FrontendLogin.html
+
+# sr_language_menu
+plugin.tx_srlanguagemenu {
+    defaultLanguageISOCode = de
+    languageTitle = 2
+#    addSelectionListHeader = 1
+}
