0d549eefb67a4cf46bfa9223caa31f838f6a55c0
David Goraj <d.goraj@exinit.de>
2017-05-29 15:55:10 +0200

    [task] edited constants.ts for multilanguage

--- typo3conf/ext/now/Configuration/TypoScript/Library/0010_config.ts
+++ typo3conf/ext/now/Configuration/TypoScript/Library/0010_config.ts
@@ -2,6 +2,7 @@ config {
     language = de
     locale_all = de_DE.utf8
     sys_language_uid = 0
+    sys_language_overlay = 1
 
     # Eindeutigere Fehlermeldungen im Frontend
     # siehe https://docs.typo3.org/typo3cms/TyposcriptReference/Setup/Config/Index.html#contentobjectexceptionhandler
