86552c3f4955cae44893ff2d4e747b21e09b5a1d
David Goraj <d.goraj@exinit.de>
2017-06-01 18:50:11 +0200

    [task] edited realurl_conf.php for URL path back to normal

--- typo3conf/realurl_conf.php
+++ typo3conf/realurl_conf.php
@@ -137,7 +137,7 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
 
 ## Domain und rootpage_id anpassen
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['begin.david.exinitdev.de'] = $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'];
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['begin.david.exinitdev.de']['pagePath']['rootpage_id'] = 17;
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['begin.david.exinitdev.de']['pagePath']['rootpage_id'] = 2;
 
 ## In der folgenden Zeile die ID der News-Detail-Seite angeben
 ## Falls mehrere Detail-Seiten verwendet werden, die Zeile kopieren und ID anpassen
