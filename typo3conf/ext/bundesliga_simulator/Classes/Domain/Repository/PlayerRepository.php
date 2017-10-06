dae116597f708b77daf8372f9b1f28cb72bba14b
David Goraj <d.goraj@exinit.de>
2017-10-06 19:11:54 +0200

    [task] edit PlayerRepository to add highestTransfer and pickSinglePlayer

--- typo3conf/ext/bundesliga_simulator/Classes/Domain/Repository/PlayerRepository.php
+++ typo3conf/ext/bundesliga_simulator/Classes/Domain/Repository/PlayerRepository.php
@@ -19,7 +19,22 @@ class PlayerRepository extends Repository
         'transferFee' => QueryInterface::ORDER_DESCENDING,
         'lastName' => QueryInterface::ORDER_ASCENDING,
         'firstName' => QueryInterface::ORDER_ASCENDING,
-        'country' => QueryInterface::ORDER_ASCENDING,
+        'nationality' => QueryInterface::ORDER_ASCENDING,
         'age' => QueryInterface::ORDER_ASCENDING,
     ];
+
+    public function highestTransfer(){
+        $query = $this->createQuery();
+        $query->getOrderings('transferFee', QueryInterface::ORDER_DESCENDING);
+
+        return $query->execute()->getFirst();
+    }
+
+    public function findSinglePlayer($player)
+    {
+        $query = $this->createQuery();
+        $query->matching(
+            $query->equals('uid', $player)
+        );
+    }
 }
