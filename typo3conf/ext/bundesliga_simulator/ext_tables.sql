518cef139fec1d270828c37abb522d5484b64090
David Goraj <d.goraj@exinit.de>
2017-10-12 15:51:26 +0200

    [task] edit tables.sql to add coach_table and add uid as coach to club_table as foreign key

--- typo3conf/ext/bundesliga_simulator/ext_tables.sql
+++ typo3conf/ext/bundesliga_simulator/ext_tables.sql
@@ -9,6 +9,55 @@ CREATE TABLE tx_bundesligasimulator_domain_model_club (
   stadium_capacity int(11) unsigned DEFAULT '0' NOT NULL,
   total_market_value int(11) unsigned DEFAULT '0' NOT NULL,
   number_of_fans int(11) unsigned DEFAULT '0' NOT NULL,
+  coach int(11) NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	CONSTRAINT coach_key FOREIGN KEY (coach) REFERENCES tx_bundesligasimulator_domain_model_coach(uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
+
+);
+
+CREATE TABLE tx_bundesligasimulator_domain_model_player (
+
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	first_name varchar(255) DEFAULT '' NOT NULL,
+	last_name varchar(255) DEFAULT '' NOT NULL,
+  birthday int(11) unsigned DEFAULT '0' NOT NULL,
+  age tinyint(2) unsigned DEFAULT '0' NOT NULL,
+	height int(3) unsigned DEFAULT '0' NOT NULL,
+	nationality varchar(255) DEFAULT '' NOT NULL,
+	position varchar(255) DEFAULT '' NOT NULL,
+	club varchar(255) DEFAULT '' NOT NULL,
+	contract int(11) unsigned DEFAULT '0' NOT NULL,
+	transfer_fee int(11) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
@@ -40,22 +89,15 @@ CREATE TABLE tx_bundesligasimulator_domain_model_club (
 	KEY language (l10n_parent,sys_language_uid)
 
 );
-
-CREATE TABLE tx_bundesligasimulator_domain_model_player (
+CREATE TABLE tx_bundesligasimulator_domain_model_coach (
 
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
-	first_name varchar(255) DEFAULT '' NOT NULL,
-	last_name varchar(255) DEFAULT '' NOT NULL,
-  birthday int(11) unsigned DEFAULT '0' NOT NULL,
-  age tinyint(2) unsigned DEFAULT '0' NOT NULL,
-	height int(3) unsigned DEFAULT '0' NOT NULL,
-	nationality varchar(255) DEFAULT '' NOT NULL,
-	position varchar(255) DEFAULT '' NOT NULL,
-	club varchar(255) DEFAULT '' NOT NULL,
-	contract int(11) unsigned DEFAULT '0' NOT NULL,
-	transfer_fee int(11) unsigned DEFAULT '0' NOT NULL,
+	name varchar(255) DEFAULT '' NOT NULL,
+	club VARCHAR(255) DEFAULT '' NOT NULL,
+	age int(11) unsigned DEFAULT '0' NOT NULL,
+
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
