90b043552083a460c30a40575ebc4dafca7cfcfd
David Goraj <d.goraj@exinit.de>
2017-10-13 12:40:18 +0200

    [task] edit tables to change coach columntype

--- typo3conf/ext/bundesliga_simulator/ext_tables.sql
+++ typo3conf/ext/bundesliga_simulator/ext_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE tx_bundesligasimulator_domain_model_club (
   stadium_capacity int(11) unsigned DEFAULT '0' NOT NULL,
   total_market_value int(11) unsigned DEFAULT '0' NOT NULL,
   number_of_fans int(11) unsigned DEFAULT '0' NOT NULL,
-  coach int(11) NOT NULL,
+  coach int(11) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
