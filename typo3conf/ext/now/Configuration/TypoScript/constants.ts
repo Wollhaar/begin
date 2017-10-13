ff105adf01c8a45bb2f5ad597fad9760ad07e878
David Goraj <d.goraj@exinit.de>
2017-10-13 15:04:19 +0200

    [task] edit constants from extension 'now' to change outcommenting style

--- typo3conf/ext/now/Configuration/TypoScript/constants.ts
+++ typo3conf/ext/now/Configuration/TypoScript/constants.ts
@@ -1,11 +1,11 @@
 # feLogin
 styles.content.loginform.templateFile = EXT:now/Resources/Private/Templates/FrontendLogin.html
 
-/*# sr_language_menu
-plugin.tx_srlanguagemenu {
-    defaultLanguageISOCode = de
-    languageTitle = 2
+# sr_language_menu
+#plugin.tx_srlanguagemenu {
+#    defaultLanguageISOCode = de
+#    languageTitle = 2
 #    addSelectionListHeader = 1
-    tx_srlanguagemenu.doNotLinkInactive = 0
-    tx_srlanguagemenu.addLinksListHeader = 1
-}*/
+#    tx_srlanguagemenu.doNotLinkInactive = 0
+#    tx_srlanguagemenu.addLinksListHeader = 1
+#}
