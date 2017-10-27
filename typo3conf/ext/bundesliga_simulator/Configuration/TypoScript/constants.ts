d8771a9ac8987baa43e128c9bbd6a045123f847d
David Goraj <d.goraj@exinit.de>
2017-10-27 15:34:39 +0200

    [task] edit constants to change Paths for view

--- typo3conf/ext/bundesliga_simulator/Configuration/TypoScript/constants.ts
+++ typo3conf/ext/bundesliga_simulator/Configuration/TypoScript/constants.ts
@@ -1,11 +1,11 @@
 plugin.tx_bundesligasimulator {
   view {
 		# cat=plugin.tx_bundesligasimulator/file; type=string; label=Path to template root (FE)
-		templateRootPaths.0 = EXT:bundesliga_simulator/Resources/Private/Templates/
+		templateRootPath = EXT:bundesliga_simulator/Resources/Private/Templates/
 		# cat=plugin.tx_bundesligasimulator/file; type=string; label=Path to template partials (FE)
-		partialRootPaths.0 = EXT:bundesliga_simulator/Resources/Private/Partials/
+		partialRootPath = EXT:bundesliga_simulator/Resources/Private/Partials/
 		# cat=plugin.tx_bundesligasimulator/file; type=string; label=Path to template layouts (FE)
-		layoutRootPaths.0 = EXT:bundesliga_simulator/Resources/Private/Layouts/
+		layoutRootPath = EXT:bundesliga_simulator/Resources/Private/Layouts/
 	}
 	persistence {
 		# cat=plugin.tx_bundesligasimulator//a; type=int+; label=Default storage PID
